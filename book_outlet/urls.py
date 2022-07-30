61597603960b3e3130710e8713b0258102b8c50b
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-07-30 09:54:57 +0530

    added listing redirect to detail page

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -3,7 +3,8 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path("", views.index)
+    path("", views.index),
+    path("<int:id>",  views.book_detail, name="book_detail")
 ]
 
 
