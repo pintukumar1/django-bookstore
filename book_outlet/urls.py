1ce82a504f19efc97884d2517611d63696bf72ff
Pintu Kumar <kumarpintu1713122@gmail.com>
2022-07-30 11:50:11 +0530

    added slug and aggregation

--- book_outlet/urls.py
+++ book_outlet/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    path("<int:id>",  views.book_detail, name="book_detail")
+    path("<slug:slug>",  views.book_detail, name="book_detail")
 ]
 
 
--- book_outlet/views.py
+++ book_outlet/views.py
@@ -1,24 +1,29 @@
 from django.http import Http404, HttpResponse, HttpResponseNotFound
 from django.shortcuts import get_object_or_404, render
 from .models import Book
+from django.db.models import Avg, Max, Min
 # Create your views here.
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-rating")
     # return HttpResponse(books)
     print("books", list(books))
+    num_books = books.count()
+    average_rating = books.aggregate(Avg("rating"))
     return render(request, "book_outlet/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books" : num_books ,
+        "average_rating" : average_rating 
     })
 
 
-def book_detail(request, id):
-    # try:
-    #     book = Book.objects.get(id = id)
-    # except:
+def book_detail(request, slug):
+    try:
+        book = Book.objects.get(slug=slug)
+    except:
     #     raise Http404()
-    book = get_object_or_404(Book, pk=id)
-        # return HttpResponseNotFound("book not found with this id.")
+    # book = get_object_or_404(Book, pk=id)
+        return HttpResponseNotFound("book not found with this id.")
     return render(request, "book_outlet/book_detail.html", {
         "title" : book.title,
         "author": book.author,
